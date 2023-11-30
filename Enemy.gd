b9b8a9d72648ebf5fd08a3e4529581528ef66ad3
Jakestermfs <152323358+Jakestermfs@users.noreply.github.com>
2023-11-30 12:49:09 -0800

    Update Enemy.gd

--- Enemy.gd
+++ Enemy.gd
@@ -1,1 +1,13 @@
-extends Node2D
+extends CharacterBody2D
+var hp = 2
+
+func _physics_process(delta):
+	if hp <= 0:
+		queue_free()
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("latk") and Globals.latk == true:
+		hp -= 1
+		print(hp)
+	
