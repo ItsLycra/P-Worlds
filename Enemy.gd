6c100a27c5834de54219c79a3d3f3144e534d014
Jakestermfs <152323358+Jakestermfs@users.noreply.github.com>
2023-12-01 09:28:13 -0800

    Update Enemy.gd

--- Enemy.gd
+++ Enemy.gd
@@ -1,15 +1,23 @@
 extends CharacterBody2D
 var hp = 2
-var movetime = true
+var movetime = false
+var speed = 100
+var negspeed = -100
 @export var gravity = 500
 
 func _physics_process(delta):
 	if hp <= 0:
 		queue_free()
-	if is_on_wall():
-		velocity.x * -1
+	if is_on_wall() and movetime == true:
+		velocity.x = speed * -1
+		speed = speed * -1
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if velocity.x > 100 and movetime == true:
+		velocity.x = speed
+	elif velocity.x < -100 and movetime == true:
+		velocity.x = negspeed
 	
 		
 	move_and_slide()
@@ -24,8 +32,10 @@ func _on_area_2d_area_entered(area):
 
 func _on_timer_timeout():
 	$AnimatedSprite2D.play("piderwalk")
-	velocity.x += 100
+	velocity.x = speed 
+	movetime = true
 	await get_tree().create_timer(1).timeout
-	velocity.x -= 100
+	velocity.x = 0
+	movetime = false
 	$AnimatedSprite2D.play("pideridle")
 	
