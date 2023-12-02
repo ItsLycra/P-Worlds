e090e1ccde9ad9e639be73147e642b2c3535b006
Jakestermfs <152323358+Jakestermfs@users.noreply.github.com>
2023-12-01 17:57:14 -0800

    Add files via upload

--- wallbreak.gd
+++ wallbreak.gd
@@ -1,5 +1,20 @@
 extends StaticBody2D
+var b1 = true
+var b2 = false
+var b3 = false
 
+func _ready():
+	$AnimatedSprite2D.play("wall")
+	
 func _on_area_2d_area_entered(area):
-	if area.is_in_group("latk"):
+	if area.is_in_group("latk") and b1 == true:
+		$AnimatedSprite2D.play("wallbreak")
+		b2 = true
+		b1 = false
+	elif area.is_in_group("latk") and b2 == true:
+		$AnimatedSprite2D.play("wallbreak2")
+		b3 = true
+		b2 = false
+	elif area.is_in_group("latk") and b3 == true:
 		queue_free()
+		b3 = false
